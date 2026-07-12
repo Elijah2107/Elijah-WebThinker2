8e9c43656ee4cbafd5878fbc2ca26e7019c6ae1b
Elijah2107 <olgawong@hotmail.com>
2026-07-12 08:46:26 +0000

    Jul 12, 2026, 8:46 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -3,18 +3,19 @@ function setup() {
   new Canvas(800,600);
   background(250);
   
-  // console.log("hello!");
-  // let base = 10
-  // let height = 5
-  // let area = 0.5 * base * height
-  // console.log(area)
-  // text(area, 100,100)
+  console.log("hello!");
+  let base = 10
+  let height = 5
+  let area = 0.5 * base * height
+  console.log(area)
+  text(area, 100,100)
 
   for (let number = 1; number <= 10; number++) {
     console.log("Counting..." + number)
   }
   // --- Exercise: Area of Triangle ---
   // write your codes here
+
 
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
