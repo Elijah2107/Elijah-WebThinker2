11780f97367b24943e96b9b5525b849f689882f0
Elijah2107 <olgawong@hotmail.com>
2026-07-12 08:37:26 +0000

    Jul 12, 2026, 8:37 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -6,7 +6,7 @@ function setup() {
   let height = 5
   let area = 0.5 * base * height
   console.log(area)
-  text(area, )
+  text(area, 100,100)
   // --- Exercise: Area of Triangle ---
   // write your codes here
 
