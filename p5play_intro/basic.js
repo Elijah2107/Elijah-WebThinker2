cbc1fbf2c6e7d87608e9e809fa5fae0877298672
Elijah2107 <olgawong@hotmail.com>
2026-07-12 09:01:10 +0000

    Jul 12, 2026, 9:01 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -25,8 +25,8 @@ function setup() {
   // --- Exercise: Age category classification ---
   // write your codes here
   
-  let age
-  
+  let age = 0
+  if (age < 9) 
   // --- Exercise: Display odd numbers backward using while loop ---
   // write your codes here
 
