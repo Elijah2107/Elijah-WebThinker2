66ac3d2d5c05e09b01469c4ec8c2a88acebfefa5
Elijah2107 <olgawong@hotmail.com>
2026-07-12 08:06:13 +0000

    Jul 12, 2026, 8:06 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -46,5 +46,7 @@ function draw() {
   if (ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2) {
     ball.vel.x *= -1;
   }
-  if (ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2)
+  if (ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2){
+    
+  }
 }
