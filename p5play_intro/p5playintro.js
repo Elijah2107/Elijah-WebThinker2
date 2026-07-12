1901349f4adcb9a9d10334ec6cec2556184bec6b
Elijah2107 <olgawong@hotmail.com>
2026-07-12 08:14:21 +0000

    Jul 12, 2026, 8:14 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -20,7 +20,7 @@ function setup() {
   ball = new Sprite();
   ball.x = 100;
   ball.y = 200;
-  ball.diameter = 40;
+  ball.diameter = 100;
   ball.color = 'blue';
   ball.vel.x = 20;
   ball.vel.y = 20;
