7184e6eb43b64335b42378133020b8bd65d56fec
Elijah2107 <olgawong@hotmail.com>
2026-07-26 09:08:18 +0000

    Jul 26, 2026, 9:08 AM

---
+++ flappybird/playground/flappybird02.js
@@ -0,0 +1,87 @@
+let bird,floor;
+let flapMidImg, bg, base;
+let flapUpImg, flapDownImg;
+let pipe;
+let bottomPipe, topPipe;
+
+function preload() {
+    flapMidImg = loadImage('assets/yellowbird-midflap.png');
+    bg = loadImage('assets/background-day.png');
+    base = loadImage('assets/base.png');
+    flapUpImg = loadImage('assets/yellowbird-upflap.png');
+    flapDownImg = loadImage('assets/yellowbird-downflap.png');
+    pipe = loadImage('assets/pipe-green.png');
+}
+function setup() {
+    new Canvas(400,600);
+    
+    bird = new Sprite();
+    bird.x = width / 2;
+    bird.y = 200;
+    bird.width = 30;
+    bird.height = 30;
+    bird.img = flapMidImg;
+
+    bird.collider = "dynamic";
+    bird.mass = 2;
+    bird.drag = 0.02;
+    bird.bounciness = 0.5;
+    world.gravity.y = 22;
+
+    floor = new Sprite();
+    floor.x = 200;
+    floor.y = height - 20;
+    floor.width = 400;
+    floor.height = 125;
+    floor.collider = "static";
+    floor.img = base;
+
+    pipeGroup = new Group();
+}
+
+function draw() {
+    image(bg, 0, 0, width, height);
+    
+    if (kb.presses('space')) {
+        bird.vel.y = -7;
+        bird.sleeping = false;
+    }
+
+    fill("blue");
+    textSize(14);
+    text('vel.y:' + bird.vel.y.toFixed(2), 10, 20);   
+
+    if (bird.vel.y < 0) {
+        bird.img = flapDownImg;
+        bird.rotation = -20;
+    } else if (bird.vel.y > 0) {
+        bird.img = flapUpImg;
+        bird.rotation = 20;
+    } else {
+        bird.img = flapMidImg;
+        bird.rotation = 0;
+    }
+
+    if (frameCount === 1) {
+        spawnRipePair();
+    }
+
+    
+}
+
+function spawnRipePair() {
+    let gap = 50;
+    let midY = height / 2;
+
+    bottomPipe = new Sprite(400, midY + gap / 2 + 200, 52, 320, 'static');
+    bottomPipe.img = pipe;
+
+    topPipe = new Sprite(400, midY - gap / 2 - 200, 52, 320, 'static');
+    topPipe.img = pipe;
+    topPipe.rotation = 180;
+
+    pipeGroup.add(bottomPipe);
+    pipeGroup.add(topPipe)
+    pipeGroup.layer = 0;
+
+}
