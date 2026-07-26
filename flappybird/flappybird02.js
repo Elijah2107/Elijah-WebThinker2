80be6b25d6fc4415bfdfc437a7274cc83171d39f
Elijah2107 <olgawong@hotmail.com>
2026-07-26 08:28:41 +0000

    Jul 26, 2026, 8:28 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -1,6 +1,6 @@
 let bird,floor;
 let flapMidImg, bg, base;
-let flapUpImg, flapDownImg
+let flapUpImg, flapDownImg;
 
 function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
