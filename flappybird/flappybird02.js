839497889a955b9b07ec0d281c95243314a67d21
Elijah2107 <olgawong@hotmail.com>
2026-07-26 09:03:40 +0000

    Jul 26, 2026, 9:03 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -76,7 +76,7 @@ function spawnRipePair() {
     bottomPipe = new Sprite(400, midY + gap / 2 + 200, 52, 320, 'static');
     bottomPipe.img = pipe;
 
-    topPipe = new Spir
+    topPipe = new Sprite(400)
 
     pipeGroup.add(bottomPipe);
     pipeGroup.add(topPipe)
