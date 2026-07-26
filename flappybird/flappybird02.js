5cc3a59af3f8eb551a672149c3887461c2ddde60
Elijah2107 <olgawong@hotmail.com>
2026-07-26 08:22:35 +0000

    Jul 26, 2026, 8:22 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -17,7 +17,7 @@ function setup() {
     bird.img = flapMidImg;
 
     bird.collider = "dynamic";
-    bird.mass = 2;
+    bird.mass = 0;
     bird.drag = 0.02;
     bird.bounciness = 0.5;
     world.gravity.y = 22;
