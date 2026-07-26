11793b29ef339437394e7e7f96c2444210b30358
Elijah2107 <olgawong@hotmail.com>
2026-07-26 08:30:01 +0000

    Jul 26, 2026, 8:30 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -7,7 +7,7 @@ function preload() {
     bg = loadImage('assets/background-day.png');
     base = loadImage('assets/base.png');
     flapUpImg = loadImage('assets/yellowbird-upflap.png');
-    flapDownImg = loadImage('assets')
+    flapDownImg = loadImage('assets/yellowbird')
 
 }
 function setup() {
