2cee419640910b654cc5fe813347716703637ec4
Elijah2107 <olgawong@hotmail.com>
2026-08-16 09:14:00 +0000

    Aug 16, 2026, 9:14 AM

--- flappybird/flappybird04.js
+++ flappybird/flappybird04.js
@@ -156,7 +156,8 @@ function draw() {
 
     // End Game on Collision
     // note that this is checking collision against the group
-    if (bird.collides(pipeGroup) || bird.collides(floor)){
+    if (bird.collides(pipeGroup) || bird.collides(floor)) {
+      
       gameoverLabel = new Sprite(width/2, height/2, 192, 42);
       gameoverLabel.img = gameoverImg;
       gameoverLabel.layer = 100; // make the game over text come to front
