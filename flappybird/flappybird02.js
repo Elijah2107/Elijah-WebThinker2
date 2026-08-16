355f41e49cbabaf3cc0b6c113dd265db9cd402a9
Elijah2107 <olgawong@hotmail.com>
2026-08-16 08:26:12 +0000

    Aug 16, 2026, 8:26 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -104,7 +104,8 @@ function drawScore(x, y, score, digitWidth, digitHeight) {
   let startX = x - totalWidth / 2;
 
   for (let i = 0; i < scoreStr.length; i++) {
-    let digit = int(scoreStr[i])
+    let digit = int(scoreStr[i]);
+    let xPos = startX
   }
 }
 
