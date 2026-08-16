5e94809813eb38f0aebce69a29deb4965772c0e6
Elijah2107 <olgawong@hotmail.com>
2026-08-16 08:34:58 +0000

    Aug 16, 2026, 8:34 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -115,7 +115,7 @@ function drawScore(x, y, score, digitWidth, digitHeight) {
   function moveGroup(group, targetX, spacing) {
     let totalWidth = (group.length -1) * spacing;
     let startX = (targetX - totalWidth/2);
-    
+
   }
 }
 
