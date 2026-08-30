9698b9e8257ee9dfcf79b52e30f316ae877edc76
Elijah2107 <olgawong@hotmail.com>
2026-08-30 09:03:49 +0000

    Aug 30, 2026, 9:03 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -62,8 +62,9 @@ function sliceFruit() {
     let d = dist(mouse.x, mouse.y, fruit.x, fruit.y);
     if (d < ((fruit.d / 2) + 5)) {
         fruit.sliced = true;
-        fruit.remove();
         
+        fruit.remove();
+
         break;
     }
 }
