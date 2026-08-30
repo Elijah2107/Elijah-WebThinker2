9edbeb5e3ab40ebe6bbced5423cbc097ebb2df9b
Elijah2107 <olgawong@hotmail.com>
2026-08-30 09:20:50 +0000

    Aug 30, 2026, 9:20 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -83,5 +83,6 @@ function sliceFruit() {
 }
 
 function splitFruit(x, y, fruitData) {
-    let left = new fruitHalves.Sprite(x - 10)
+    let left = new fruitHalves.Sprite(x - 10, y, 40, 40);
+    left.img
 }
