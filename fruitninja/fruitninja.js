040a4e2d33eb085b5ac1e935675299f87d2c2fd4
Elijah2107 <olgawong@hotmail.com>
2026-09-20 08:58:34 +0000

    Sep 20, 2026, 8:58 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -67,7 +67,8 @@ function draw() {
         fill(0, 180);
         rect(0, 0, width, height);
         fill(255, 0, 0);
-        textAlign
+        textAlign(CENTER, CENTER);
+        
     }
 
     if (frameCount % 60 === 0) {
