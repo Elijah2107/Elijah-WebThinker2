d07e3797625d8c0cb5666eb67512859617d54857
Elijah2107 <olgawong@hotmail.com>
2026-09-20 08:59:48 +0000

    Sep 20, 2026, 8:59 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -72,7 +72,7 @@ function draw() {
         text('Game Over!', width / 2, height / 2 - 60);
         textSize(24);
         fill(255);
-        text('Score: ' + score )
+        text('Score: ' + score, width / 2,)
     }
 
     if (frameCount % 60 === 0) {
