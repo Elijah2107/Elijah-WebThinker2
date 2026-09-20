a4c8cba52b0dfc74644222a181d628c3c9c8ceb2
Elijah2107 <olgawong@hotmail.com>
2026-09-20 09:21:45 +0000

    Sep 20, 2026, 9:21 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -31,6 +31,7 @@ function preload() {
 
     fruitTypes = [peach, watermelon];
     sliceSound = loadSound('assets/fruit-ninja-combo.mp3');
+    backgroundTrack = loadSound('assets/fruit-ninja')
 }
 
 function setup() {
