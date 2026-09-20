52aa0fa35eef2c3af0110831e97b75d62f6e9dd9
Elijah2107 <olgawong@hotmail.com>
2026-09-20 08:23:48 +0000

    Sep 20, 2026, 8:23 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -51,7 +51,8 @@ function draw() {
         textSize(48);
         text('Fruit Ninja', width / 2, height / 2 - 40);
         textSize(24);
-        text('Press SPACE or Click to Start', width / 2, height / 2)
+        text('Press SPACE or Click to Start', width / 2, height / 2 + 20);
+        return;
     }
 
     if (frameCount % 60 === 0) {
