60e178da57be435e43b110ae27cb705ec145ca68
Elijah2107 <olgawong@hotmail.com>
2026-09-20 08:21:56 +0000

    Sep 20, 2026, 8:21 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -46,7 +46,8 @@ function draw() {
     if (gameState === 'start') {
         fill(0,180);
         rect(0, 0, width, height);
-        
+        fill(255);
+        t
     }
 
     if (frameCount % 60 === 0) {
